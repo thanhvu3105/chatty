df682912c429eead01f30ffc8343eae148ce689f
Julian Vu <tpv4@txstate.edu>
2022-03-28 19:39:48 -0500

    Fix some bugs

--- Chatty/main.java
+++ Chatty/main.java
@@ -5,11 +5,11 @@ public class main{
     public static void main(String[] args){
         System.out.println("HELLOOO");
         Server server = new Server();
-        User user1 = new User("John");
+        User user1 = new User("Luke the Molester");
         server.AddUser(user1);
-        User user2 = new User("Matt");
+        User user2 = new User("Matt the Fucked");
         server.AddUser(user2);
-        User user3 = new User("Crab");
+        User user3 = new User("Gay Lord");
         server.AddUser(user3);
     }
 }
